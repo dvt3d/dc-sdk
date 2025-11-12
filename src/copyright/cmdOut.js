02535f8bc181360aef097c1b6f17d3624987a2d3
cavencj <cavencj@gmail.com>
2025-11-12 15:22:38 +0800

    update cesium info

--- src/copyright/cmdOut.js
+++ src/copyright/cmdOut.js
@@ -6,7 +6,7 @@
 console.log(
   `%c \n DC-SDK \n  %c \n 用数字描绘世界之美  %c \n
     版本: {{__VERSION__}} - {{__TIME__}}
-    Cesium Engine 版本：{{__ENGINE_VERSION__}}
+    Cesium 版本：{{__CESIUM_VERSION__}}
     开发作者: {{__AUTHOR__}}
     网站主页: {{__HOME_PAGE__}}
     github: {{__REPOSITORY__}}
